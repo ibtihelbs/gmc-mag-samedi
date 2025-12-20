ccb7b7940acf926a46b6a8a5927f3579ad29fa4b
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-12-20 10:42:54 +0100

    js data

---
+++ html-course/script.js
@@ -0,0 +1,44 @@
+let intructorName = "ibtihel ben salah r788698698td22 @^#&@^";
+let numberone = 16.5;
+numberone = 30;
+console.log(8 * 5);
+console.log(intructorName);
+console.log(numberone + 5);
+console.log(5 > 10);
+let hello = true;
+console.log(hello);
+let x = 5;
+let y = "5";
+console.log(x === y);
+const students = [
+  "Roufiada",
+  "Siwar",
+  "Maryem",
+  "Dial",
+  "Ibtissem",
+  "Ahmed",
+  "Othmen",
+  "sara",
+];
+const presence = [
+  "PRESENTE",
+  "PRESENTE",
+  "PRESENTE",
+  "PRESENT",
+  "abscente",
+  "abscent",
+  "abscent",
+  "abscente",
+];
+
+console.log(students[1], presence[1]);
+console.log(students[7], presence[7]);
+
+const roufaida = {
+  firstName: "Rofaida",
+  lastName: "Guissous",
+  presence: true,
+  progress: 35,
+};
+console.log(roufaida.firstName);
+console.log(roufaida.presence);
